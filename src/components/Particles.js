e212e754600009b56c6a0047fc7d99a38ab83239
Sam Ngiam <you@example.com>
2023-12-05 01:49:57 -0500

    Adding files

---
+++ src/components/Particles.js
@@ -0,0 +1,105 @@
+import { useCallback } from "react";
+import Particles from "react-particles";
+import { loadFull } from "tsparticles";
+
+const AppParticles = () => {
+  const particlesInit = useCallback(async (engine) => {
+    console.log(engine);
+    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
+
+  return (
+    <Particles
+      className='z-0'
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        fullScreen: true,
+        background: {
+          color: {
+            value: "#191616"
+          }
+        },
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+               mode: "push"
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true
+          },
+          modes: {
+            push: {
+              quantity: 2,
+            },
+            repulse: {
+              distance: 40,
+              factor: 2,
+            }
+          }
+        },
+        particles: {
+          color: {
+            value: '#437b7b'
+          },
+        //   00AA95
+        // #437b7b
+        //229ECB
+        //479292
+          links: {
+            color: '#437b7b',
+            distance: 150,
+            enable: true,
+            opacity: 0.4,
+            width: 1
+          },
+          collisions: {
+            enable: true
+          },
+          move: {
+            directions: "none",
+            enable: true,
+            outModes: {
+              default: "bounce"
+            },
+            random: false,
+            speed: 0.175,
+            straight: false
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 100
+            },
+            value: 20
+          },
+          opacity: {
+            value: 0.3
+          },
+          shape: {
+            type: "circle"
+          },
+          size: {
+            value: { min: 0, max: 0 }
+          }
+        },
+        detectRetina: true
+      }}
+    />
+  );
+};
+
+export default AppParticles;
---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+const About = () => {
+    return (
+    <div className="grow flex text-white flex-col justify-center items-start px-64 z-50 font-fira">
+        <p className='text-6xl mb-14 animate-fade-right font-quicksand'>About</p>
+        <p className='text-2xl mb-10' align="left">
+        Hi there! I'm Sam, a student at the University of Pennsylvania. I'm currently pursuing a Bachelor's of Science in Engineering in Computer Science, with minors in Data Science and Mathematics.
+        
+         </p>
+         <p className='text-2xl mb-10' align="left">
+         I've always been fascinated by the power of technology to solve tough problems and shape the future. 
+         This has led me to explore diverse engineering experiences addressing challenges in a variety of fields<code>&#8212;</code> from gravitational waves to swarm drones to multi-agent pathfinding.
+        </p>
+         <p className='text-2xl' align="left">In my free time, you can find me climbing rocks, reading books, or looking for the best food trucks.
+         </p>
+    </div>
+    )
+}
+
+export default About;
