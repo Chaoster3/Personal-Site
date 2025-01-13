7db4bc859f03ad16f07438bf47db7c662a215a68
Chaoster3 <samuelngiam@yahoo.com>
2025-01-12 19:14:04 -0500

    Testing again

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ const Home = () => {
     return (
     <div className="grow flex flex-col justify-center z-50 text-white">
         <p className='text-7xl mb-20 mt-20 animate-fade-right font-quicksand font'>Hi, I'm Sam</p>
-        <h2 className='text-3xl m-20 font-quicksand'>A student</h2>
+        <h2 className='text-3xl m-20 font-quicksand'>A student at</h2>
     </div>
     )
 };
