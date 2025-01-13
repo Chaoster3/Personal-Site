2f8b5b243b2cbc6e2f0241d596db2937caa4c24d
Chaoster3 <samuelngiam@yahoo.com>
2025-01-12 19:27:45 -0500

    Small changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,7 @@ const About = () => {
     <div className="grow flex text-white flex-col justify-center items-start px-64 z-50 font-quicksand">
         <p className='text-6xl mb-14 animate-fade-right font-quicksand'>About</p>
         <p className='text-2xl mb-10' align="left">
-        Hi there, I'm Sam! I'm currently pursuing a Bachelor's of Science in Engineering in Computer Science at UPenn's School of Engineering and Applied Science and a Bachelor's of Science in Economics at the Wharton School.
+        Hi there, I'm Sam! I'm currently a student at the University of Pennsylvania pursuing a Bachelor's of Science in Engineering in Computer Science at the School of Engineering and Applied Science and a Bachelor's of Science in Economics at the Wharton School.
         
          </p>
          <p className='text-2xl mb-10' align="left">
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,8 @@
 const Home = () => {
     return (
     <div className="grow flex flex-col justify-center z-50 text-white">
-        <p className='text-7xl mb-20 mt-20 animate-fade-right font-quicksand font'>Hi, I'm Sam</p>
-        <h2 className='text-3xl m-20 font-quicksand'>A student at</h2>
+        <p className='text-7xl mb-20 mt-20 animate-fade-right font-quicksand font'>Sam Ngiam</p>
+        <h2 className='text-3xl m-20 font-quicksand'>CS + Finance @ University of Pennsylvania</h2>
     </div>
     )
 };
